5a2ca661ffdecfa01aad6eebdc3a001686535c4e
VladiSH31 <vladislav.sheremet@ukr.net>
2025-09-24 17:23:23 +0300

    feat(MoviesPages): start migrated movies pages

--- src/components/header-component/HeaderComponent.tsx
+++ src/components/header-component/HeaderComponent.tsx
@@ -1,14 +1,13 @@
+"use client"
 import './HeaderComponent.css'
 import LogoComponent from "@/components/header-component/logo-component/LogoComponent";
 import MenuComponent from "@/components/header-component/menu-component/MenuComponent";
 import SearchComponent from "@/components/header-component/search-component/SearchComponent";
 import UserInfoComponent from "@/components/header-component/user-info-component/UserInfoComponent";
-// import SearchComponent from "./search-component/SearchComponent.tsx";
-// import UserInfoComponent from "./user-info-component/UserInfoComponent.tsx";
-// import {useState} from "react";
+import {useState} from "react";
 
 const HeaderComponent = () => {
-    // const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
         <header className="app-header relative">
@@ -31,28 +30,28 @@ const HeaderComponent = () => {
                         <UserInfoComponent />
                     </div>
 
-                    {/*/!*Меню гамбургер*!/*/}
-                    {/*<button*/}
-                    {/*    className="hamburger-button"*/}
-                    {/*    onClick={() => setIsMenuOpen(!isMenuOpen)}*/}
-                    {/*    aria-expanded={isMenuOpen}*/}
-                    {/*    aria-controls="mobile-menu"*/}
-                    {/*>*/}
-                    {/*    <span className="sr-only">Open main menu</span>*/}
-                    {/*    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">*/}
-                    {/*        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />*/}
-                    {/*    </svg>*/}
-                    {/*</button>*/}
+                    {/*Меню гамбургер*/}
+                    <button
+                        className="hamburger-button"
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        aria-expanded={isMenuOpen}
+                        aria-controls="mobile-menu"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
                 </div>
 
             </div>
 
-            {/*/!* Мобільне меню *!/*/}
-            {/*{isMenuOpen && (*/}
-            {/*    <div id="mobile-menu" className="mobile-menu-container">*/}
-            {/*        <MenuComponent />*/}
-            {/*    </div>*/}
-            {/*)}*/}
+            {/* Мобільне меню */}
+            {isMenuOpen && (
+                <div id="mobile-menu" className="mobile-menu-container">
+                    <MenuComponent />
+                </div>
+            )}
 
         </header>
     );
