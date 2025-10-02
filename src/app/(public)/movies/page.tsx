36aca49ea411526a72c0d1d618a777fa981b5bae
VladiSH31 <vladislav.sheremet@ukr.net>
2025-10-02 11:55:17 +0300

    feat(movies): refactor MoviesComponent for prop-based data and remove Redux dependencies

--- src/app/(public)/movies/page.tsx
+++ src/app/(public)/movies/page.tsx
@@ -2,7 +2,8 @@ import './MoviesPage.css'
 import MoviesComponent from "@/components/movies-component/MoviesComponent";
 import PaginationComponent from "@/components/pagination-component/PaginationComponent";
 import {FC} from "react";
-import {moviesService} from "@/services/global.api.service";
+import {genreService, moviesService} from "@/services/global.api.service";
+import {IGenreMovies} from "@/models/IGenreMovies";
 
 type Props = {
     searchParams: { [key: string]: string | string[] | undefined }
@@ -13,9 +14,16 @@ const MoviesPage: FC<Props> = async ({searchParams}) => {
     const page = Number(searchParams.page || '1');
     const moviesData = await moviesService.getMovies(page);
 
+    const selectedGenreId = Number(searchParams.genre || '')
+    const moviesGenre: IGenreMovies[] = await genreService.getMoviesGenre();
+
+    const selectedGenre = moviesGenre.find(genre => genre.id === selectedGenreId)
+    const pageTitle = selectedGenre ? `Showing results for: ${selectedGenre.name}`
+        : "All Movies";
+
     return (
         <div>
-            <MoviesComponent movies={moviesData.results} />
+            <MoviesComponent movies={moviesData.results} pageTitle={pageTitle}/>
             <PaginationComponent totalPages={moviesData.total_pages} currentPage={page}/>
         </div>
     );
